e09edd504f475e91cd50d33fd326cbd8f930d3e6
svetlanakoroleva <svetlanakoroleva312@gmail.com>
2023-03-03 00:01:27 +0000

    added subnets

--- startup/variable.tf
+++ startup/variable.tf
@@ -12,6 +12,24 @@ variable "region" {
 
 variable "tags" {
   description = "Please provide a tag for resource name"
-  type        = map
+  type        = map(any)
   default     = {}
+}
+
+variable "public_subnet1" {
+  description = "Please provide subnet CIDR block"
+  type        = string
+  default     = ""
+}
+
+variable "public_subnet2" {
+  description = "Please provide subnet CIDR block"
+  type        = string
+  default     = ""
+}
+
+variable "public_subnet3" {
+  description = "Please provide subnet CIDR block"
+  type        = string
+  default     = ""
 }
