548756f9bd3856e78b259ef2a86c4a165eb5e868
svetlanakoroleva <svetlanakoroleva312@gmail.com>
2023-03-03 22:51:00 +0000

    added rout tables for private

--- startup/main.tf
+++ startup/main.tf
@@ -89,3 +89,29 @@ resource "aws_route_table_association" "public3" {
   subnet_id      = aws_subnet.public3.id
   route_table_id = aws_route_table.rt.id
 }
+
+resource "aws_route_table" "rt_private" {   # create rt for private associate
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+  tags                 = var.tags
+
+}
+
+resource "aws_route_table_association" "private1" {   
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.rt_private.id      # attach to your rt
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.rt_private.id
+}
+
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.rt_private.id
+}
