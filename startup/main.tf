aa39370de001d7d53a5e1b215ae2f9f89c673cc0
svetlanakoroleva <svetlanakoroleva312@gmail.com>
2023-03-03 23:04:55 +0000

    added outputs

--- startup/main.tf
+++ startup/main.tf
@@ -6,6 +6,7 @@ resource "aws_vpc" "main" {
   tags                 = var.tags
 }
 
+
 resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet1
@@ -26,6 +27,7 @@ resource "aws_subnet" "public3" {
   tags                    = var.tags
   map_public_ip_on_launch = true # Specify true to indicate that instances launched into the subnet should be assigned a public IP address
 }
+
 
 
 resource "aws_subnet" "private1" {
@@ -54,64 +56,65 @@ resource "aws_internet_gateway" "gw" {
 }
 
 
-resource "aws_eip" "nat" {
-  vpc      = true
-}
+# resource "aws_eip" "nat" {
+#   vpc      = true
+# }
 
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public1.id
-  depends_on = [aws_internet_gateway.gw]
-}
+# resource "aws_nat_gateway" "example" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public1.id
+#   depends_on = [aws_internet_gateway.gw]
+# }
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "rt" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags                 = var.tags
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+#   tags                 = var.tags
 
-}
+# }
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.rt.id
-}
+# resource "aws_route_table_association" "public1" {
+#   subnet_id      = aws_subnet.public1.id
+#   route_table_id = aws_route_table.rt.id
+# }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.rt.id
-}
+# resource "aws_route_table_association" "public2" {
+#   subnet_id      = aws_subnet.public2.id
+#   route_table_id = aws_route_table.rt.id
+# }
 
-resource "aws_route_table_association" "public3" {
-  subnet_id      = aws_subnet.public3.id
-  route_table_id = aws_route_table.rt.id
-}
+# resource "aws_route_table_association" "public3" {
+#   subnet_id      = aws_subnet.public3.id
+#   route_table_id = aws_route_table.rt.id
+# }
 
-resource "aws_route_table" "rt_private" {   # create rt for private associate
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "rt_private" {   # create rt for private associate
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.example.id
-  }
-  tags                 = var.tags
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.example.id
+#   }
+#   tags                 = var.tags
 
-}
+# }
 
-resource "aws_route_table_association" "private1" {   
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.rt_private.id      # attach to your rt
-}
+# resource "aws_route_table_association" "private1" {   
+#   subnet_id      = aws_subnet.private1.id
+#   route_table_id = aws_route_table.rt_private.id      # attach to your rt
+# }
 
-resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.rt_private.id
-}
+# resource "aws_route_table_association" "private2" {
+#   subnet_id      = aws_subnet.private2.id
+#   route_table_id = aws_route_table.rt_private.id
+# }
 
-resource "aws_route_table_association" "private3" {
-  subnet_id      = aws_subnet.private3.id
-  route_table_id = aws_route_table.rt_private.id
-}
+# resource "aws_route_table_association" "private3" {
+#   subnet_id      = aws_subnet.private3.id
+#   route_table_id = aws_route_table.rt_private.id
+# }
+
