c1cf01e55a64938747bf5f7c0cbddf9cc6d6eace
svetlanakoroleva <svetlanakoroleva312@gmail.com>
2023-03-06 19:00:59 +0000

    outputs

---
+++ RDS/module/main.tf
@@ -0,0 +1,21 @@
+module rds {
+    source = "../"
+    region               = "us-east-1"
+    allocated_storage    = 20
+    db_name              = "mydb"
+    engine               = "mysql"
+    engine_version       = "5.7"
+    instance_class       = "db.t3.micro"
+    username             = "foo"
+    password             = "foobarbaz"
+    publicly_accessible  = true
+        
+    tags = {
+        Name = "main"
+    }
+}
+
+
+output endpoint {
+    value = module.rds.endpoint
+}
